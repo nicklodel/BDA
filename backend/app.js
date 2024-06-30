e31b6444e1deda63dcb33eeaa081405f267107c2
Nicklodel <nicolasjlopezdelgado@gmail.com>
2024-06-30 09:16:10 +0200

    nocred

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,10 @@ app.use(cors());
 // MySQL Code goes here
 const pool = mysql.createConnection({
     connectionLimit : 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'Nicklodel01?',
-    database: 'BDA'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 })
 
 // Get all beers
